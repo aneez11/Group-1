2ced745330bb309b93f8f3a5f3127f5e7f256f2e
Paras Rai <paras.rai38@gmail.com>
2019-06-05 11:59:13 +0545

    modified footer,store n merged

---
+++ store.php
@@ -0,0 +1,108 @@
+<?php include("header.html"); ?>
+<div class="container-fluid main-box">
+	<div class="white-paper mt-3 p-4 w-100">
+		<div class="container-fluid row">
+			
+				<h3 class="sujanh1" >Stores on Pricena</h3>
+				<div class="search-box float-right">
+					<input type="text" name="search" placeholder="Find a Store">
+				</div>
+		</div>
+
+		<hr>
+		<div class="sujanrow row">
+			<div class= "col-2">
+				<a class="sujana" href="#">
+					<div class="logo-box">
+						<img class="img-fluid rounded-circle  border border-dark w-100 h-100 mx-auto d-block img-box" src="images/store_logos/amazonae logo.png">
+					</div>
+				</a>
+				<a class="sujana"class="sujana" href="#">Amazon AE</a>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star"></span>
+				<span class="fa fa-star"></span>
+			</div>
+
+			<div class= "col-2">
+				<a class="sujana" href="#"><div class="logo-box">
+					<img class="img-fluid rounded-circle w-100 h-100 mx-auto d-block img-box" src="images/store_logos/mamas_papas.png">
+				</div></a>
+				<a class="sujana"href="#">Mamas & Papas</a>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star"></span>
+				<span class="fa fa-star"></span>
+			</div>
+
+			<div class= "col-2">
+				<a class="sujana" href="#"><div class="logo-box">
+					<img class="img-fluid rounded-circle w-100 h-100 mx-auto d-block img-box" src="images/store_logos/noon-logo.png">
+				</div></a>
+				<a class="sujana" href="#">Noon</a>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star"></span>
+				<span class="fa fa-star"></span>
+			</div>
+
+			<div class= "col-2">
+				<a class="sujana" href="#"><div class="logo-box">
+					<img class="img-fluid rounded-circle w-100 h-100  mx-auto d-block img-box" src="images/store_logos/sharafdg_logo.png">
+				</div></a>
+				<a class="sujana" href="#">Sharaf DG</a>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star"></span>
+				<span class="fa fa-star"></span>
+			</div>
+
+			<div class= "col-2">
+				<a class="sujana" href="#"><div class="logo-box">
+					<img class="img-fluid rounded-circle w-100 h-100  mx-auto d-block img-box" src="images/store_logos/sprii-new-logo.png">
+				</div></a>
+				<a class="sujana"href="#">Sprii</a>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star"></span>
+				<span class="fa fa-star"></span>
+			</div>
+
+			<div class= "col-2">
+				<a class="sujana" href="#"><div class="logo-box">
+					<img class="img-fluid rounded-circle w-100 h-100  mx-auto d-block img-box" src="images/store_logos/virgin_logo2.png">
+				</div></a>
+				<a class="sujana"href="#">Virgin Megastone</a>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star"></span>
+				<span class="fa fa-star"></span>
+			</div>
+		</div>
+
+		<div class="sujanrow">
+			<div class= "col-2">
+				<a class="sujana" href="#"><div class="logo-box">
+					<img class="img-fluid rounded-circle w-100 h-100  mx-auto d-block img-box" src="images/store_logos/namshi.png">
+				</div></a>
+				<a class="sujana" href="#">Namashi</a>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star checked"></span>
+				<span class="fa fa-star"></span>
+				<span class="fa fa-star"></span>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+
+
+<?php include("footer.html"); ?>
