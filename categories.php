5c842ec06bf63c40e6fb503e2018423ab6b06715
dhlpradip <dhlpradip12@gmail.com>
2019-06-05 12:03:18 +0545

    Done

---
+++ categories.php
@@ -0,0 +1,129 @@
+	<?php include("header.html"); ?>
+	<div class="container-fluid mt-3 ml-1">
+	<div class="white-paper" id="all-categories">
+		<h1>Categories</h1>
+		<div class="row">
+		<div class="col" id="mobile-tablets">
+			<span class="title"><a href="/mobile-tablets">Mobiles, Tablets &amp; Wearables</a></span>
+			<ul>
+				<li id="mobile-phones"><a href="/mobile-tablets/mobile-phones">Mobile Phones </a></li>
+				<li id="tablets-e-readers"><a href="/mobile-tablets/tablets-e-readers">Tablets &amp; EReaders </a></li>
+				<li id="mobile-tablets-accessories"><i></i><a href="/mobile-tablets/mobile-tablets-accessories">Mobile &amp; Tablets Accessories </a>
+					<ul>
+						<li id="cases-covers-skins"><a href="/mobile-tablets/mobile-tablets-accessories/cases-covers-skins">Cases, Covers &amp; Skins</a></li>
+						<li id="screen-protectors"><a href="/mobile-tablets/mobile-tablets-accessories/screen-protectors">Screen Protectors</a></li>
+						<li id="docks-batteries-chargers"><a href="/mobile-tablets/mobile-tablets-accessories/docks-batteries-chargers">Docks, Batteries &amp; Chargers</a></li>
+						<li id="cables-adapters"><a href="/mobile-tablets/mobile-tablets-accessories/cables-adapters">Cables &amp; Adapters</a></li>
+						<li id="other-gadgets"><a href="/mobile-tablets/mobile-tablets-accessories/other-gadgets">Other gadgets</a></li>
+						<li id="keyboards-controllers"><a href="/mobile-tablets/mobile-tablets-accessories/keyboards-controllers">Keyboards &amp; Controllers</a></li>
+						<li id="memory-cards"><a href="/mobile-tablets/mobile-tablets-accessories/memory-cards">Memory Cards</a></li>
+						<li id="pens-styluses"><a href="/mobile-tablets/mobile-tablets-accessories/pens-styluses">Pens &amp; Styluses</a></li>
+						<li id="giftcards"><a href="/mobile-tablets/mobile-tablets-accessories/giftcards">Giftcards</a></li>
+						<li id="selfie-sticks"><a href="/mobile-tablets/mobile-tablets-accessories/selfie-sticks">Selfie Sticks</a></li>
+					</ul>
+				</li>
+				<li id="wearables">
+					<i></i><a href="/mobile-tablets/wearables">Wearables </a>
+					<ul>
+						<li id="armbands-wristbands"><a href="/mobile-tablets/wearables/armbands-wristbands">Armbands &amp; Wristbands</a></li>
+						<li id="smart-watches"><a href="/mobile-tablets/wearables/smart-watches">Smart Watches</a></li>
+						<li id="vr-headsets"><a href="/mobile-tablets/wearables/vr-headsets">VR Headsets</a></li>
+					</ul>
+				</li>
+			</ul>
+
+		</div>
+		<div class="col" id="mobile-tablets">
+			<span class="title"><a href="/mobile-tablets">Mobiles, Tablets &amp; Wearables</a></span>
+			<ul>
+				<li id="mobile-phones"><a href="/mobile-tablets/mobile-phones">Mobile Phones </a></li>
+				<li id="tablets-e-readers"><a href="/mobile-tablets/tablets-e-readers">Tablets &amp; EReaders </a></li>
+				<li id="mobile-tablets-accessories"><i></i><a href="/mobile-tablets/mobile-tablets-accessories">Mobile &amp; Tablets Accessories </a>
+					<ul>
+						<li id="cases-covers-skins"><a href="/mobile-tablets/mobile-tablets-accessories/cases-covers-skins">Cases, Covers &amp; Skins</a></li>
+						<li id="screen-protectors"><a href="/mobile-tablets/mobile-tablets-accessories/screen-protectors">Screen Protectors</a></li>
+						<li id="docks-batteries-chargers"><a href="/mobile-tablets/mobile-tablets-accessories/docks-batteries-chargers">Docks, Batteries &amp; Chargers</a></li>
+						<li id="cables-adapters"><a href="/mobile-tablets/mobile-tablets-accessories/cables-adapters">Cables &amp; Adapters</a></li>
+						<li id="other-gadgets"><a href="/mobile-tablets/mobile-tablets-accessories/other-gadgets">Other gadgets</a></li>
+						<li id="keyboards-controllers"><a href="/mobile-tablets/mobile-tablets-accessories/keyboards-controllers">Keyboards &amp; Controllers</a></li>
+						<li id="memory-cards"><a href="/mobile-tablets/mobile-tablets-accessories/memory-cards">Memory Cards</a></li>
+						<li id="pens-styluses"><a href="/mobile-tablets/mobile-tablets-accessories/pens-styluses">Pens &amp; Styluses</a></li>
+						<li id="giftcards"><a href="/mobile-tablets/mobile-tablets-accessories/giftcards">Giftcards</a></li>
+						<li id="selfie-sticks"><a href="/mobile-tablets/mobile-tablets-accessories/selfie-sticks">Selfie Sticks</a></li>
+					</ul>
+				</li>
+				<li id="wearables">
+					<i></i><a href="/mobile-tablets/wearables">Wearables </a>
+					<ul>
+						<li id="armbands-wristbands"><a href="/mobile-tablets/wearables/armbands-wristbands">Armbands &amp; Wristbands</a></li>
+						<li id="smart-watches"><a href="/mobile-tablets/wearables/smart-watches">Smart Watches</a></li>
+						<li id="vr-headsets"><a href="/mobile-tablets/wearables/vr-headsets">VR Headsets</a></li>
+					</ul>
+				</li>
+			</ul>
+
+		</div>
+		<div class="col" id="mobile-tablets">
+			<span class="title"><a href="/mobile-tablets">Mobiles, Tablets &amp; Wearables</a></span>
+			<ul>
+				<li id="mobile-phones"><a href="/mobile-tablets/mobile-phones">Mobile Phones </a></li>
+				<li id="tablets-e-readers"><a href="/mobile-tablets/tablets-e-readers">Tablets &amp; EReaders </a></li>
+				<li id="mobile-tablets-accessories"><i></i><a href="/mobile-tablets/mobile-tablets-accessories">Mobile &amp; Tablets Accessories </a>
+					<ul>
+						<li id="cases-covers-skins"><a href="/mobile-tablets/mobile-tablets-accessories/cases-covers-skins">Cases, Covers &amp; Skins</a></li>
+						<li id="screen-protectors"><a href="/mobile-tablets/mobile-tablets-accessories/screen-protectors">Screen Protectors</a></li>
+						<li id="docks-batteries-chargers"><a href="/mobile-tablets/mobile-tablets-accessories/docks-batteries-chargers">Docks, Batteries &amp; Chargers</a></li>
+						<li id="cables-adapters"><a href="/mobile-tablets/mobile-tablets-accessories/cables-adapters">Cables &amp; Adapters</a></li>
+						<li id="other-gadgets"><a href="/mobile-tablets/mobile-tablets-accessories/other-gadgets">Other gadgets</a></li>
+						<li id="keyboards-controllers"><a href="/mobile-tablets/mobile-tablets-accessories/keyboards-controllers">Keyboards &amp; Controllers</a></li>
+						<li id="memory-cards"><a href="/mobile-tablets/mobile-tablets-accessories/memory-cards">Memory Cards</a></li>
+						<li id="pens-styluses"><a href="/mobile-tablets/mobile-tablets-accessories/pens-styluses">Pens &amp; Styluses</a></li>
+						<li id="giftcards"><a href="/mobile-tablets/mobile-tablets-accessories/giftcards">Giftcards</a></li>
+						<li id="selfie-sticks"><a href="/mobile-tablets/mobile-tablets-accessories/selfie-sticks">Selfie Sticks</a></li>
+					</ul>
+				</li>
+				<li id="wearables">
+					<i></i><a href="/mobile-tablets/wearables">Wearables </a>
+					<ul>
+						<li id="armbands-wristbands"><a href="/mobile-tablets/wearables/armbands-wristbands">Armbands &amp; Wristbands</a></li>
+						<li id="smart-watches"><a href="/mobile-tablets/wearables/smart-watches">Smart Watches</a></li>
+						<li id="vr-headsets"><a href="/mobile-tablets/wearables/vr-headsets">VR Headsets</a></li>
+					</ul>
+				</li>
+			</ul>
+
+		</div>
+		<div class="col" id="mobile-tablets">
+			<span class="title"><a href="/mobile-tablets">Mobiles, Tablets &amp; Wearables</a></span>
+			<ul>
+				<li id="mobile-phones"><a href="/mobile-tablets/mobile-phones">Mobile Phones </a></li>
+				<li id="tablets-e-readers"><a href="/mobile-tablets/tablets-e-readers">Tablets &amp; EReaders </a></li>
+				<li id="mobile-tablets-accessories"><i></i><a href="/mobile-tablets/mobile-tablets-accessories">Mobile &amp; Tablets Accessories </a>
+					<ul>
+						<li id="cases-covers-skins"><a href="/mobile-tablets/mobile-tablets-accessories/cases-covers-skins">Cases, Covers &amp; Skins</a></li>
+						<li id="screen-protectors"><a href="/mobile-tablets/mobile-tablets-accessories/screen-protectors">Screen Protectors</a></li>
+						<li id="docks-batteries-chargers"><a href="/mobile-tablets/mobile-tablets-accessories/docks-batteries-chargers">Docks, Batteries &amp; Chargers</a></li>
+						<li id="cables-adapters"><a href="/mobile-tablets/mobile-tablets-accessories/cables-adapters">Cables &amp; Adapters</a></li>
+						<li id="other-gadgets"><a href="/mobile-tablets/mobile-tablets-accessories/other-gadgets">Other gadgets</a></li>
+						<li id="keyboards-controllers"><a href="/mobile-tablets/mobile-tablets-accessories/keyboards-controllers">Keyboards &amp; Controllers</a></li>
+						<li id="memory-cards"><a href="/mobile-tablets/mobile-tablets-accessories/memory-cards">Memory Cards</a></li>
+						<li id="pens-styluses"><a href="/mobile-tablets/mobile-tablets-accessories/pens-styluses">Pens &amp; Styluses</a></li>
+						<li id="giftcards"><a href="/mobile-tablets/mobile-tablets-accessories/giftcards">Giftcards</a></li>
+						<li id="selfie-sticks"><a href="/mobile-tablets/mobile-tablets-accessories/selfie-sticks">Selfie Sticks</a></li>
+					</ul>
+				</li>
+				<li id="wearables">
+					<i></i><a href="/mobile-tablets/wearables">Wearables </a>
+					<ul>
+						<li id="armbands-wristbands"><a href="/mobile-tablets/wearables/armbands-wristbands">Armbands &amp; Wristbands</a></li>
+						<li id="smart-watches"><a href="/mobile-tablets/wearables/smart-watches">Smart Watches</a></li>
+						<li id="vr-headsets"><a href="/mobile-tablets/wearables/vr-headsets">VR Headsets</a></li>
+					</ul>
+				</li>
+			</ul>
+
+		</div>
+	</div>
+</div>
+</div>
+<?php include("footer.html"); ?>
